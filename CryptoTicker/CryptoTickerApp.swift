1cc4efbd21c7e96a23973a41ac28402236f379a9
Razvan Rujoiu <rjrazvan@icloud.com>
2021-10-29 11:01:25 +0300

    Initial Commit

---
+++ CryptoTicker/CryptoTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTickerApp.swift
+//  CryptoTicker
+//
+//  Created by Razvan Rujoiu on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
